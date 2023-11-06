51f3ef33ea8a90bdd0b09b97ea24cdc16ddc9dca
Larissa Nicoli <larisnicoli@gmail.com>
2023-11-06 14:54:18 -0300

    component creation

---
+++ src/Pagination/index.tsx
@@ -0,0 +1,41 @@
+interface PaginationProps {
+  previousPage?: string;
+  onHandlePrevious: VoidFunction;
+  onHandleNext: VoidFunction;
+}
+
+export const Pagination = ({
+  onHandleNext,
+  onHandlePrevious,
+  previousPage,
+}: PaginationProps) => {
+  return (
+    <>
+      <label
+        className={`
+        mr-2
+        ${
+          previousPage !== null
+            ? "text-blue-600 hover:text-blue-800 cursor-pointer"
+            : "text-gray-500"
+        }`}
+        onClick={previousPage !== null ? onHandlePrevious : () => {}}
+      >
+        {"<<"} Anterior
+      </label>
+      <label className="text-blue-600 dark:text-white">||</label>
+      <label
+        onClick={onHandleNext}
+        className={`
+  ml-2
+  ${
+    previousPage !== null
+      ? "text-blue-600 hover:text-blue-800 cursor-pointer"
+      : "text-gray-500"
+  }`}
+      >
+        Próximo {">>"}
+      </label>
+    </>
+  );
+};
