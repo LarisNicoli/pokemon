1dc91031ac9a29e153e1cfebc7cb52566a2b43a3
Larissa Nicoli <larisnicoli@gmail.com>
2023-11-08 15:02:11 -0300

    darkmode

---
+++ src/Toggle/index.tsx
@@ -0,0 +1,27 @@
+interface ToggleProps {
+  isDark: boolean;
+  handleDark: VoidFunction;
+}
+
+export const Toggle = ({ isDark, handleDark }: ToggleProps) => {
+  return (
+    <div
+      className="
+  h-5
+  w-12
+  rounded-lg
+  bg-gray-400
+  cursor-pointer
+  relative
+  dark:bg-gray-800
+  "
+      onClick={handleDark}
+    >
+      <div
+        className={`routend-2xl h-full w-6 bg-white absolute  ${
+          isDark ? "right-0" : "left-0"
+        }`}
+      ></div>
+    </div>
+  );
+};
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -6,6 +6,9 @@ import { PokemonService } from "../../services/pokemon";
 import { Loader } from "../../components/Loader";
 import { Card } from "../../components/Card";
 import { Pagination } from "../../Pagination";
+import { Toggle } from "../../Toggle";
+import { useAtom } from "jotai";
+import { useIsDark } from "../../hooks/useThemes";
 
 export const Home = () => {
   const [pokemons, setPokemons] = useState<SimplePokemon[]>([]);
@@ -13,6 +16,8 @@ export const Home = () => {
   const [nextPage, setNextPage] = useState("");
   const [previousPage, setPreviousPage] = useState<string | undefined>("");
   const [pokemon, setPokemon] = useState<Pokemon>();
+
+  const [isDark, setIsDark] = useAtom(useIsDark);
 
   const getPokemons = useCallback(async (page: string) => {
     setIsLoading(true);
@@ -59,36 +64,54 @@ export const Home = () => {
     }
   };
 
+  const handleDarkMode = () => {
+    const newDarkValue = !isDark;
+    setIsDark(newDarkValue);
+    sessionStorage.setItem("isDark", newDarkValue.toString());
+  };
+
+  useEffect(() => {
+    if (sessionStorage.getItem("isDark")) {
+      const isDarkMode = sessionStorage.getItem("isDark");
+      setIsDark(isDarkMode === "true" ? true : false);
+    }
+  }, [setIsDark]);
+
   return (
-    <div className="h-full m-full bg-blue-100">
-      <Header title="Pokédex">
-        <Input onHandleSearch={onHandleSearch} />
-      </Header>
+    <div className={`h-full m-full bg-blue-100 ${isDark ? "dark" : ""}`}>
+      <div className="dark:bg-gray-700">
+        <Header title="Pokédex">
+          <Input onHandleSearch={onHandleSearch} />
+          <div className="absolute top-[150px] md:top-[70px] md:bottom-2 left-5">
+            <Toggle isDark={isDark} handleDark={handleDarkMode} />
+          </div>
+        </Header>
 
-      {isLoading ? (
-        <div className="flex justify-center items-center h-screen">
-          <Loader />
-        </div>
-      ) : (
-        <main className="mt-5 md:mt-10 flex justify-center flex-wrap">
-          {!pokemon &&
-            pokemons.map((pokemon) => (
-              <Card key={pokemon.name} simplePokemon={pokemon} />
-            ))}
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <Loader />
+          </div>
+        ) : (
+          <main className="mt-5 md:mt-10 flex justify-center flex-wrap">
+            {!pokemon &&
+              pokemons.map((pokemon) => (
+                <Card key={pokemon.name} simplePokemon={pokemon} />
+              ))}
 
-          {pokemon && <Card pokemonSearched={pokemon} />}
-        </main>
-      )}
+            {pokemon && <Card pokemonSearched={pokemon} />}
+          </main>
+        )}
 
-      {!pokemon && (
-        <footer className="flex w-full justify-center p-5 text-xl">
-          <Pagination
-            previousPage={previousPage}
-            onHandleNext={handleNextPage}
-            onHandlePrevious={handlePreviousPage}
-          />
-        </footer>
-      )}
+        {!pokemon && (
+          <footer className="flex w-full justify-center p-5 text-xl">
+            <Pagination
+              previousPage={previousPage}
+              onHandleNext={handleNextPage}
+              onHandlePrevious={handlePreviousPage}
+            />
+          </footer>
+        )}
+      </div>
     </div>
   );
 };
